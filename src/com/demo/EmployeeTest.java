eac9e10f39426fe89d2cf06837c701ba09259d4d
Raghava <raghava.krishnamaraju@gmail.com>
2023-01-31 12:56:11 +0530

    adding new emp object

--- src/com/demo/EmployeeTest.java
+++ src/com/demo/EmployeeTest.java
@@ -6,7 +6,7 @@ public class EmployeeTest {
         /* Create two objects using constructor */
         Employee empOne = new Employee("James Smith");
         Employee empTwo = new Employee("Mary Anne");
-
+        Employee empThree = new Employee("Mary Anne");
         // Invoking methods for each object created
         empOne.empAge(26);
         empOne.empDesignation("Senior Software Engineer");
@@ -17,5 +17,10 @@ public class EmployeeTest {
         empTwo.empDesignation("Software Engineer");
         empTwo.empSalary(500);
         empTwo.printEmployee();
+
+        empThree.empAge(28);
+        empThree.empDesignation("Architect Engineer");
+        empThree.empSalary(500000);
+        empThree.printEmployee();
     }
 }
